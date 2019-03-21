9f70537928f6e5649e758efe9a8c00e74d6aa404
Karmen <lu.kar@husky.neu.edu>
2019-03-21 19:30:39 -0400

    Added deploy.sh, service, nginx, start.sh files

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4796
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/tasks3/bin/tasks3 stop || true
+
+echo "Starting app..."
+
+# Start to run in background from shell.
+#_build/prod/rel/tasks3/bin/tasks3 start
+
+# Foreground for testing and for systemd
+_build/prod/rel/tasks3/bin/tasks3 foreground
+
+
+
