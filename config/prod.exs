9129013202d651c25ced37830cabdaa0bf152b7b
Karmen <lu.kar@husky.neu.edu>
2019-03-21 19:47:38 -0400

    Update host url

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :tasks3, Tasks3Web.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "tasks3.karmen.website", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
